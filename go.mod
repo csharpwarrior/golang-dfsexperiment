0471c728e84698a61f100eb84b9008a8f4140325
Артем Розанов <a.rozanov@dataphone-russia.com>
2023-01-24 16:07:24 +0300

    test main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-codinggames-dfs-exit-nodes
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hi")
+	//generate dfs
+	
+
+}
+func createbinarytree
